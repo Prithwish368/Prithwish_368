ea2d906505c2a3a89b9e3733a9c3b799cefba96b
Prithwish Mitra Pithu <pithumitra20@gmail.com>
2025-10-29 13:36:35 +0600

    Add files via upload

--- array3.c
+++ array3.c
@@ -1,27 +1,32 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int number[]={9,8,7,4,5,6,3};
+    int number[]={5,7,8,6,4,2};
+    int la=sizeof (number)/sizeof (number[0]);
+    if (la<2)
+        printf("we need more number\n");
+    int fast,secend;
+    if (number[0]>number[1])
+    {
+        fast =number[0];
+        secend=number[1];
+    }
+    else {fast=number[1];
+        secend=number[0];
+    }
+    for (int i=2;i<la;i++)
+    {
+        if(fast<number[i])
+        {
+            secend=fast;
+            fast=number[i];
+        }
+        else if (secend<number[i]&&number[i]!=fast)
+            secend=number[i];
 
-        int max=number[0];
-            int min=number[0];
-            for (int i=1;i<7;i++)
-            {
-                if (max<number[i])
-                {
-                    max=number[i];
-                }
-            }
-            printf("max = %d",max);
+    }
 
-             for (int i=1;i<7;i++)
-            {
-                if (min>number[i])
-                {
-                    min=number[i];
-                }
-            }
-            printf("min = %d",min);
+    printf("fast number =%d\n",fast);
+    printf("2nd number = %d",secend);
     return 0;
-
 }
